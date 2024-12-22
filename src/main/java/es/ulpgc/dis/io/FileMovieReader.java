90fa92ca87fbb5098dcc7c7824ea9d77b522962f
Wafatriki <azdadwafa0@gmail.com>
2024-12-22 21:12:20 +0000

    FileMovieReader implemented

---
+++ src/main/java/es/ulpgc/dis/io/FileMovieReader.java
@@ -0,0 +1,39 @@
+package es.ulpgc.dis.io;
+
+import software.ulpgc.kata4.model.Movie;
+
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class FileMovieReader implements MovieReader, AutoCloseable {
+    private final MovieDeserializer deserializer;
+    private final BufferedReader reader;
+
+    public FileMovieReader(File file, MovieDeserializer deserializer) throws IOException {
+        this.deserializer = deserializer;
+        ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(file));
+        ZipEntry entry = zipInputStream.getNextEntry();
+        this.reader = new BufferedReader(new InputStreamReader(zipInputStream));
+        if (entry != null) {
+            this.reader.readLine();
+        }
+    }
+
+
+    @Override
+    public Movie read() throws IOException {
+        return deserialize(reader.readLine());
+    }
+
+    private Movie deserialize(String line) {
+        return line != null ?
+                deserializer.deserialize(line) :
+                null;
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.reader.close();
+    }
+}
