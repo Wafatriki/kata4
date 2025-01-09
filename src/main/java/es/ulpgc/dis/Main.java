ea1b92faab9324b5459d0b0c358eb524d5191c58
Wafatriki <azdadwafa0@gmail.com>
2025-01-09 19:20:15 +0000

    Main implemented

--- src/main/java/es/ulpgc/dis/Main.java
+++ src/main/java/es/ulpgc/dis/Main.java
@@ -1,6 +1,6 @@
 package es.ulpgc.dis;
 
-import software.ulpgc.kata4.commands.ImportCommand;
+import es.ulpgc.dis.commands.ImportCommand;
 
 public class Main {
     public static void main(String[] args) throws Exception {
